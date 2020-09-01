6bad6867b27ef7eda040cbf296b06c446627143d
Maido Kaara <maido@producement.com>
2020-09-01 21:49:24 +0300

    Changes

--- main.js
+++ main.js
@@ -1,63 +1,72 @@
 const initialState = {
-    field1: [6,17,35,54,71,13,30,40,60,74,5,19,36,59,67,10,29,42,52,69,1,23,43,49,62],
-    field2: [7,21,35,56,74,6,25,43,60,66,8,28,40,46,70,10,29,45,57,73,5,20,36,55,62],
+    tables: new Map([
+        [1, new Map([6, 17, 35, 54, 71, 13, 30, 40, 60, 74, 5, 19, 36, 59, 67, 10, 29, 42, 52, 69, 1, 23, 43, 49, 62].map((i) => [i, false]))],
+        [2, new Map([7, 21, 35, 56, 74, 6, 25, 43, 60, 66, 8, 28, 40, 46, 70, 10, 29, 45, 57, 73, 5, 20, 36, 55, 62].map((i) => [i, false]))]
+    ]),
 };
 
-const table1 = document.querySelectorAll("table")[0];
-const table2 = document.querySelectorAll("table")[1];
+const tables = document.querySelectorAll("table");
 
 const initState = (initialState) => {
     const storedState = localStorage.getItem("state");
-    if(storedState) {
-        const {table1, table2} = JSON.parse(storedState);
-        for (i = 0; i < table1.length; i++) {
-            if(table1[i]) {
-                n.style.backgroundColor = "white";
-            } else {
-                n.style.backgroundColor = "red";
-            }
-        }
-    }
-    else {
+    if (storedState) {
+        return storedState;
+    } else {
         return initialState;
     }
 };
 
-const state = initState(initialState);
+let state = initState(initialState);
 
-const populateTable = (table, content) => {
-    content.forEach((value, i) => {
-        let tr;
-        if(i % 5 === 0) {
+const populateTable = (tableNr, table, content) => {
+    let tr, i = 0;
+    content.forEach((checked, key) => {
+        if (i % 5 === 0) {
             tr = document.createElement("tr");
             table.appendChild(tr);
         }
         const td = document.createElement("td");
-        td.innerText = value;
+        td.innerText = key;
+        td.setAttribute("data-cell", i);
+        td.setAttribute("data-table", tableNr);
+        if (checked) {
+            td.style.backgroundColor = "red";
+        } else {
+            td.style.backgroundColor = "white";
+        }
         tr.appendChild(td);
+        i++;
     });
 };
 
-populateTable(table1, state.field1);
-populateTable(table2, state.field2);
+const draw = (state) => {
+    tables.forEach((table, i) => populateTable(i, table, state.tables.get(i)));
+};
 
 const reducer = (action, state) => {
-    switch(action.name) {
+    switch (action.name) {
+        case 'INIT':
+            return {...state};
         case 'CHECK':
-            return {...state}; 
-        case 'UNCHECK':
-            return {...state}
+            const entries = state.tables.entries();
+
+            const cell = table.get(action.cell);
+            return {...state, tables: []};
     }
     return {...state};
 };
 
+const action = (action) => {
+    state = reducer(action, state);
+    draw(state);
+};
+
+action('INIT');
+
 document.querySelectorAll("td").forEach((n) => {
     n.onclick = () => {
-        const current = n.style.backgroundColor;
-        if(current === 'red') {
-            n.style.backgroundColor = "white";
-        } else {
-            n.style.backgroundColor = "red";
-        }
+        const cell = n.getAttribute("data-cell");
+        const table = n.getAttribute("data-table");
+        action('CHECK', {table, cell});
     }
 });
